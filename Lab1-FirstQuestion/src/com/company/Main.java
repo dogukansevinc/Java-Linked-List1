ea2d9947f9b047ce4ccd85be627880938ac9bdba
Mehmet Doğukan Sevinç <65046472+dogukansevinc@users.noreply.github.com>
2021-11-07 18:57:32 +0300

    Add files via upload

---
+++ Lab1-FirstQuestion/src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	MyLinkedList list = new MyLinkedList();
+
+    list.add(2);
+    list.add(0);
+    list.add(1);
+    list.add(8);
+    list.add(0);
+    list.add(2);
+    list.add(2);
+    list.add(1);
+    list.add(7);
+    list.printList();
+
+    list.delete(2);
+    list.printList();
+
+    list.removeAt(2);
+    list.printList();
+
+    list.removeAt(3);
+    list.printList();
+
+
+    }
+}
